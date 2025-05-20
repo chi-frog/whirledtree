efccedd80eb5f1bb4fcaa2e8c2c37f7ca2d02829
whirledtree <133811180+whirledtree@users.noreply.github.com>
2025-05-21 00:00:47 +0200

    basic pixel art drawing

---
+++ src/components/PixelArtMaker.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useState } from 'react';
+
+const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_GRID_COLOR = "#fff";
+const DEFAULT_SELECTED_COLOR = "#000";
+
+export default function PixelArtMaker() {
+  const [gridSize, setGridSize] = useState<number>(DEFAULT_GRID_SIZE);
+  const [grid, setGrid] = useState<string[]>(
+    Array(gridSize * gridSize).fill(DEFAULT_GRID_COLOR)
+  );
+  const [selectedColor, setSelectedColor] = useState<string>(
+    DEFAULT_SELECTED_COLOR);
+
+  const handleCellClick = (index: number) => {
+    const newGrid = [...grid];
+    newGrid[index] = selectedColor;
+    setGrid(newGrid);
+  }
+
+  const handleGridChange = (newGridSize: number) => {
+    setGridSize(newGridSize);
+    setGrid(Array(newGridSize * newGridSize).fill(DEFAULT_GRID_COLOR));
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <div className="flex gap-3">
+        <label>Pick Grid Size:</label>
+        <input
+          type="number"
+          value={gridSize}
+          className="text-black bg-white"
+          onChange={(e) => handleGridChange(+e.target.value)} />
+        <label>Pick Color:</label>
+        <input
+          type="color"
+          value={selectedColor}
+          onChange={(e) => setSelectedColor(e.target.value)} />
+      </div>
+      <div className="grid w-[30rem] h-[30rem]" style={{
+        gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+      }}>
+        {grid.map((color, index) => (
+          <div
+            key={index}
+            onClick={() => handleCellClick(index)}
+            className="border"
+            style={{ backgroundColor: color}}>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
