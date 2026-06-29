7cbe6283696b8c539c306ef10d0de71cf5a9c4d3
whirledtree <133811180+whirledtree@users.noreply.github.com>
2026-06-29 18:37:21 +0200

    oracle text helper text enabled

--- src/components/magic/OracleText.tsx
+++ src/components/magic/OracleText.tsx
@@ -3,6 +3,16 @@
 import { MagicSymbol } from "@/hooks/magic/useMagicSymbols";
 import { useMemo } from "react";
 
+enum MagicTextType {
+  NORMAL = 'normal',
+  HELPER = 'helper',
+}
+type MagicText = {
+  type:MagicTextType,
+  content:string,
+}
+const isMagicText = (obj:MagicText|MagicSymbol) => ('type' in obj);
+
 type Props = {
   oracleText:string,
   symbols:MagicSymbol[],
@@ -11,19 +21,48 @@ type Props = {
 const OracleText:React.FC<Props> = ({oracleText, symbols, symbolImageMap}) => {
   const paragraphs = oracleText.split('\n');
   
-  type Paragraph = (string|MagicSymbol)[];
+  type Paragraph = (MagicText|MagicSymbol)[];
   type TokenizeParagraph = (paragraph:string)=>Paragraph;
   const tokenizeParagraph:TokenizeParagraph = (paragraph) => {
+    const magicParagraph = {type:MagicTextType.NORMAL, content:paragraph};
+    let helperTextTokens:MagicText[] = [];
+
+    const openingParenthesis = magicParagraph.content.indexOf('(');
+    if (openingParenthesis < 0) helperTextTokens = [magicParagraph];
+
+    const endingParenthesis = magicParagraph.content.indexOf(')');
+    if (endingParenthesis < 0) {
+      console.log('Weird! No Ending', magicParagraph);
+      helperTextTokens = [magicParagraph];
+
+    } else {
+      const start = magicParagraph.content.substring(0, openingParenthesis);
+      const helper = magicParagraph.content.substring(openingParenthesis, endingParenthesis + 1);
+      const end = magicParagraph.content.substring(endingParenthesis + 1);
+        
+      if (start !== "") helperTextTokens.push({
+        type:MagicTextType.NORMAL,
+        content: start});
+      helperTextTokens.push({
+        type:MagicTextType.HELPER,
+        content:helper});
+      if (end !== "") helperTextTokens.push({
+        type:MagicTextType.NORMAL,
+        content:end});
+    }
+
     const relevantSymbols = symbols.filter(({symbol}) =>
                               paragraph.includes(symbol));
 
     const tokenizedParagraph = relevantSymbols.reduce<Paragraph>(
       (remaining, symbol) => {
+        console.log('Remaining', remaining);
+
         const parts = remaining.map((part) =>
-          (typeof part === 'string' ||
-           part instanceof String) ? part.split(symbol.symbol) :
-                                    [part]);
-                                    console.log('Parts', parts);
+          isMagicText(part) ? part.content.split(symbol.symbol).map(
+                                (_part) => ({type:part.type, content:_part})) :
+                              [part]);
+        console.log('Parts', parts);
         const partsExpanded = parts.map((part) => {
           if (part.length <= 1) return part;
           let result = part.reduce<Paragraph>((expanded, phrase) =>
@@ -33,11 +72,12 @@ const OracleText:React.FC<Props> = ({oracleText, symbols, symbolImageMap}) => {
           result.splice(result.length - 1);
           return result;
         });
+        console.log('partsExpanded', partsExpanded);
         const partsFlat = partsExpanded.flat();
         console.log('partsFlat', partsFlat);
 
-        return partsFlat.filter((part) => part !== '');
-      }, [paragraph]);
+        return partsFlat.filter((part) => !isMagicText(part) || part.content !== '');
+      }, helperTextTokens);
 
     return tokenizedParagraph;
   }
@@ -45,19 +85,20 @@ const OracleText:React.FC<Props> = ({oracleText, symbols, symbolImageMap}) => {
   const tokenizedParagraphs = paragraphs.map(tokenizeParagraph);
   console.log('tokenizedParagraphs', tokenizedParagraphs);
 
-  const transformToken:(token:string|MagicSymbol, index:number)=>React.ReactNode = (token, index) => {
-    if ((typeof token === 'string') ||
-        (token instanceof String)) {
-      return (<span key={index}>{token}</span>);
-    } else {
-      const imageUri = token.imageUri;
+  const transformToken:(token:MagicText|MagicSymbol, index:number)=>React.ReactNode = (token, index) => {
+    if (isMagicText(token))
+      return (token.type === MagicTextType.NORMAL) ?
+        (<span key={index*10} className="normalText">{token.content}</span>) :
+        (<span key={index*10} className="helperText" style={{
+          fontStyle:'italic'}}>
+          {token.content}</span>);
+    else
       return (
-        <img key={index} src={imageUri}  alt="" className="icon" style={{
+        <img key={index} src={token.imageUri}  alt="" className="icon" style={{
           width:'16px',
           margin:'1px',
           display:'inline',
         }}/>);
-    }
   };
 
   const transformParagraph:(paragraph:Paragraph, index:number)=>React.ReactNode = (paragraph, index) => {
